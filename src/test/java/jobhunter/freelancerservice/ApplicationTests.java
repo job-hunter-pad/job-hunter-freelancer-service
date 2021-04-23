607dab49635746c56b03c35c59e071d51928d4f5
Andrei Timar <atimar999@gmail.com>
2021-04-23 15:43:00 +0300

    Initial Commit

---
+++ src/test/java/jobhunter/freelancerservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jobhunter.freelancerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
